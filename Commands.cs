d8bc7334d05a93883a8c0727c70c5680586d53a4
EndMyCode <82176345+EndMyCode@users.noreply.github.com>
2021-04-08 20:34:03 +0200

    Add files via upload

---
+++ Commands.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CliWrap;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Audio;
+
+namespace DiscordBot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("help")]
+        [Summary("Tells you the commands")]
+        public async Task Help()
+        {
+            var info = new EmbedFieldBuilder()
+                .WithName("Infos")
+                .WithValue("If you want to use a multi word argument, you have to put it in quotation marks. (example: ;nick @User \"Name Goes Here\"")
+                .WithIsInline(true);
+            var command1 = new EmbedFieldBuilder()
+                .WithName("help")
+                .WithValue("Shows commands.")
+                .WithIsInline(true);
+            var command2 = new EmbedFieldBuilder()
+                .WithName("test")
+                .WithValue("Tests if the bot is working, by repeating what you said.")
+                .WithIsInline(true);
+            var command3 = new EmbedFieldBuilder()
+                .WithName("insult")
+                .WithValue("Insults someone.")
+                .WithIsInline(true);
+            var command4 = new EmbedFieldBuilder()
+                .WithName("nick (nickname)")
+                .WithValue("Changes the nickname of a person(requires the change nickname permission.")
+                .WithIsInline(true);
+            var embed = new EmbedBuilder()
+                .WithAuthor(Context.Client.CurrentUser)
+                .AddField(info)
+                .AddField(command1)
+                .AddField(command2)
+                .AddField(command3)
+                .AddField(command4)
+                .Build();
+
+            await ReplyAsync(embed: embed);
+
+
+        }
+
+        [Command("test")]
+        [Summary("If the bot doesnt react, use this command to see if its enabled.")]
+        public Task TestAsync([Remainder][Summary("The said text.")] string echo)
+            => ReplyAsync(echo);
+
+        [Command("insult")]
+        [Summary("Insults someone.")]
+        public async Task BeleidigungAsync([Summary("The insulted user.")] SocketGuildUser user = null)
+        {
+            await user.SendMessageAsync($"{Context.Message.Author} hat mir gesagt dass ich dir sagen soll, du sollst dich ficken.");
+        }
+
+        [Command("nick")]
+        [Summary("changes nickname.")]
+        [Alias("nickname")]
+        [RequireUserPermission(GuildPermission.ChangeNickname)]
+        public async Task NickAsync([Summary("User to rename.")] SocketGuildUser user = null, [Summary("The new name.")] string name = null)
+        {
+            await user.ModifyAsync(x =>
+            {
+                x.Nickname = name;
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Discord;
+using Discord.Net;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using YoutubeExplode;
+using YoutubeExplode.Channels;
+using System.Collections.Generic;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        public CommandService _commands;
+        private IServiceProvider _services;
+        private Values value;
+
+        public static void Main(string[] args)
+            => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public async Task RunBotAsync()
+        {
+            var config = new DiscordSocketConfig
+            {
+                AlwaysDownloadUsers = true
+            };
+
+            _client = new DiscordSocketClient(config);
+            _commands = new CommandService();
+
+            _services = new ServiceCollection().AddSingleton(_client).AddSingleton(_commands).BuildServiceProvider();
+
+            string token = value.token;
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix(";", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                    await context.Channel.SendMessageAsync($"An error occured. Reason: {result.ErrorReason}");
+                }
+            }
+            #region responses
+            if (message.Content.Contains("bot") || message.Content.Contains("Bot")) {
+                if (message.Content.Contains("shit") || message.Content.Contains("Shit"))
+                {
+                    if (message.Content.Contains("not")) return;
+                    await context.Channel.SendMessageAsync($"Your mom is shit, {context.User.Username}");
+                }
+                if (message.Content.Contains("trash") || message.Content.Contains("Trash"))
+                {
+                    if (message.Content.Contains("not")) return;
+                    await context.Channel.SendMessageAsync($"Your mom is trash, {context.User.Username}");
+                }
+                if (message.Content.Contains("sucks") || message.Content.Contains("Sucks"))
+                {
+                    if (message.Content.Contains("not")) return;
+                    await context.Channel.SendMessageAsync($"Your mom sucks, {context.User.Username}");
+                }
+                if(message.Content.Contains("stupid") || message.Content.Contains("Stupid"))
+                {
+                    if (message.Content.Contains("not")) return;
+                    await context.Channel.SendMessageAsync($"Yo momma's stupid, {context.User.Username}");
+                }
+            }
+            #endregion
+        }
+    }
+}
